ec2f014637b48e2c9e8a8073c17852dadb7f32cc
unknown <analara714@gmail.com>
2024-04-20 01:05:18 -0300

    chore: estilizando o texto do footer

--- src/components/FooterContent.vue
+++ src/components/FooterContent.vue
@@ -1,3 +1,12 @@
 <template>
-  <span>Desenvolvido por: <a>AnaLara714</a></span>
+  <span
+    >Desenvolvido por: <a href="https://github.com/AnaLara714" target="_blank">AnaLara714</a></span
+  >
 </template>
+
+<style>
+span a {
+  color: #000000;
+  font-style: italic;
+}
+</style>
